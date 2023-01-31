f3d9bc482ea3f4144ccbabd6b9a86918c819321d
asrafmi <asraf.muhammad07@gmail.com>
2023-01-31 17:27:05 +0700

    feat : add data kecamatan & kelurahan

---
+++ db/provinsi.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql:3306
+-- Generation Time: Jan 31, 2023 at 10:19 AM
+-- Server version: 5.7.40
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pemilu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provinsi`
+--
+
+CREATE TABLE `provinsi` (
+  `id_provinsi` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `provinsi`
+--
+
+INSERT INTO `provinsi` (`id_provinsi`, `nama`) VALUES
+(1, 'Aceh'),
+(2, 'Sumatera Utara'),
+(3, 'Sumatera Barat'),
+(4, 'Riau'),
+(5, 'Jambi'),
+(6, 'Sumatera Selatan'),
+(7, 'Bengkulu'),
+(8, 'Lampung'),
+(9, 'Kepulauan Bangka Belitung'),
+(10, 'Kepulauan Riau'),
+(11, 'Dki Jakarta'),
+(12, 'Jawa Barat'),
+(13, 'Jawa Tengah'),
+(14, 'Di Yogyakarta'),
+(15, 'Jawa Timur'),
+(16, 'Banten'),
+(17, 'Bali'),
+(18, 'Nusa Tenggara Barat'),
+(19, 'Nusa Tenggara Timur'),
+(20, 'Kalimantan Barat'),
+(21, 'Kalimantan Tengah'),
+(22, 'Kalimantan Selatan'),
+(23, 'Kalimantan Timur'),
+(24, 'Kalimantan Utara'),
+(25, 'Sulawesi Utara'),
+(26, 'Sulawesi Tengah'),
+(27, 'Sulawesi Selatan'),
+(28, 'Sulawesi Tenggara'),
+(29, 'Gorontalo'),
+(30, 'Sulawesi Barat'),
+(31, 'Maluku'),
+(32, 'Maluku Utara'),
+(33, 'Papua Barat'),
+(34, 'Papua');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `provinsi`
+--
+ALTER TABLE `provinsi`
+  ADD PRIMARY KEY (`id_provinsi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `provinsi`
+--
+ALTER TABLE `provinsi`
+  MODIFY `id_provinsi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
